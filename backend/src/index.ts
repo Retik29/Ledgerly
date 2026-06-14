f551e0f121aed86a0bf549d96ad8a15bbd2b91ac
Retik Yadav <retiknyadav614@gmail.com>
2026-06-14 21:02:34 +0530

    fix: updated cors origins and allowed preflight

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,10 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173", // default Vite react port
+  origin: [
+    "http://localhost:5173",
+    "https://ledgerly-frontend-six.vercel.app"
+  ],
   credentials: true
 }));
 app.use(express.json({ limit: "10mb" })); // support large CSV content payloads
